4b69e9478107d441052feebbdf5727c3598b2414
Evan LaBate <49501559+JonGrayson@users.noreply.github.com>
2021-01-24 15:28:52 -0700

    Add files via upload

---
+++ Scripts/AIBehaviours.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AIBehaviours : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+
+    public string AIStatus;
+
+    public Transform playerTarget;
+
+    public Rigidbody rig;
+
+    Vector3 direction;
+
+    Vector3 moveDirection;
+
+    private void Update()
+    {
+        if(AIStatus == "Flee")
+        {
+            Flee();
+        }
+
+        else if(AIStatus == "Seek")
+        {
+            Seek();
+        }
+    }
+
+    public void Flee()
+    {
+        direction = transform.position - playerTarget.position;
+        transform.rotation = Quaternion.LookRotation(direction);
+        transform.position += transform.TransformDirection(Vector3.forward) * moveSpeed * Time.deltaTime;
+        //rig.AddForce(moveDirection);
+    }
+
+    public void Seek()
+    {
+        direction = playerTarget.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(direction);
+        transform.position += transform.TransformDirection(Vector3.forward) * moveSpeed * Time.deltaTime;
+
+        //moveDirection = direction.normalized * moveSpeed * Time.deltaTime;
+        //transform.LookAt(moveDirection);
+        //rig.AddForce(moveDirection);
+    }
+}
